f9571ab67e71db3799222282b8fed137ef03abfe
Anna Razina <vega_b@mail.ru>
2023-01-23 21:35:16 +0400

    счётчик с помощью классового и функционального компонентов

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, {useState} from "react";
+import Counter from "./components/counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
- const [likes, setLikes] = useState(0);
+ const [value, setValue] = useState("текст в инпуте");
  
- function Increment () {
-  setLikes(likes + 1)
- };
- function Decrement () {
-  setLikes(likes - 1)
- };
+ 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={Increment}>Increment</button>
-      <button onClick={Decrement}>Decrement</button>
+      <h1>{value}</h1>
+      <input 
+        type = "text" 
+        value = {value}
+        onChange = {event => setValue(event.target.value)}
+      />
+      
+      <Counter/>
+      <ClassCounter/>
+      
     </div>
   );
 }
