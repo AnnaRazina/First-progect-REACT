06de1d3791d48e751686b7d9bec9dea7d9e804bc
Anna Razina <vega_b@mail.ru>
2023-01-31 08:42:25 +0400

    props - списком с указанием ключа

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem"
+import Post from "./components/Post"
 import "./style/App.css";
 
 
 function App() {
  const [value, setValue] = useState("текст в инпуте");
- 
+ const [posts, setPosts] = useState([
+  {id: 1, title: "JavaScript-1", body: "descripcion"},
+  {id: 2, title: "JavaScript-2", body: "descripcion"},
+  {id: 3, title: "JavaScript-3", body: "descripcion"},
+ ])
  
   return (
     <div className="App">
@@ -20,9 +25,17 @@ function App() {
       
       <Counter/>
       <ClassCounter/>
+
       <PostItem id={1} title="JavaScript" body="descripcion"/>
       <PostItem id={2} title="PHP" body="descripcion"/>
       <PostItem id={3} title="Python" body="descripcion"/>
+
+      {posts.map(post => 
+        <Post post={post} key={post.id}/>
+
+      )}
+
+      
 
     
       
